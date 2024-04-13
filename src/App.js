6dfc3fddd7bd00540f08b1ac2da10b9c241a8be0
Marcio <marciodosanjos@ymail.com>
2024-04-13 10:22:56 -0300

    projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h2 className="text-2xl text-red-500">Welcome to Tailwind</h2>
+    </>
+  );
+}
+
+export default App;
