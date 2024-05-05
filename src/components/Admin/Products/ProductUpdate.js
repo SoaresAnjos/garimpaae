f87d1680ca373802d1764ba9206a546d72bae11d
Marcio <marciodosanjos@ymail.com>
2024-05-05 05:40:55 -0300

    ajuste update produto

--- src/components/Admin/Products/ProductUpdate.js
+++ src/components/Admin/Products/ProductUpdate.js
@@ -1,62 +1,41 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import Select from "react-select";
 import makeAnimated from "react-select/animated";
+
 import ErrorMsg from "../../ErrorMsg/ErrorMsg";
 import LoadingComponent from "../../LoadingComp/LoadingComponent";
 import SuccessMsg from "../../SuccessMsg/SuccessMsg";
 import {
-  addProductAction,
-  fecthProductsAction,
   fetchProductAtion,
   updateProductAction,
 } from "../../../redux/slices/products/productsSlice";
-import { fetchCategoriesAction } from "../../../redux/slices/categories/categoriesSlice";
 import { fetchBrandsAction } from "../../../redux/slices/brands/brandsSlice";
 import { fetchColorsAction } from "../../../redux/slices/colors/colorsSlice";
-import { useParams } from "react-router-dom";
+import { fetchCategoriesAction } from "../../../redux/slices/categories/categoriesSlice";
 
 //animated components for react-select
 const animatedComponents = makeAnimated();
 
 export default function ProductUpdate() {
+  //dispatch
   const dispatch = useDispatch();
-
   //get id from params
   const { id } = useParams();
+  //fetch single product
+  useEffect(() => {
+    dispatch(fetchProductAtion(id));
+  }, [id, dispatch]);
 
-  // //files
-  // const [files, setFiles] = useState([]);
-  // const [filesError, setFilesError] = useState([]);
-
-  // //file handle change
-  // const fileHandleChange = (event) => {
-  //   const newFiles = Array.from(event.target.files);
-
-  //   const newErrs = [];
-  //   //validation
-  //   newFiles.forEach((file) => {
-  //     if (file?.size > 1000000) {
-  //       newErrs.push(`${file.name} é muito grande`);
-  //     }
-  //     if (!file?.type?.startsWith("image/")) {
-  //       newErrs.push(`${file.name} não é uma imagem `);
-  //     }
-  //   });
-
-  //   setFiles(newFiles);
-  //   setFilesError(newErrs);
-  // };
-
-  //sizes
-  const sizes = ["S", "M", "L", "XL"];
+  //Sizes
+  const sizes = ["S", "M", "L", "XL", "XXL"];
   const [sizeOption, setSizeOption] = useState([]);
-
   const handleSizeChange = (sizes) => {
     setSizeOption(sizes);
   };
-
-  const sizeOptionsCoverted = sizes.map((size) => {
+  //converted sizes
+  const sizeOptionsCoverted = sizes?.map((size) => {
     return {
       value: size,
       label: size,
@@ -69,9 +48,11 @@ export default function ProductUpdate() {
   }, [dispatch]);
 
   //select categories data from store
-  const { categories, loading, error } = useSelector(
-    (state) => state?.categories
-  );
+  const {
+    categories,
+    loading: loadingCategory,
+    error: errorCategory,
+  } = useSelector((state) => state?.categories);
 
   //brands
   useEffect(() => {
@@ -82,108 +63,85 @@ export default function ProductUpdate() {
   const { brands } = useSelector((state) => state?.brands);
 
   //colors
-  const [colorOption, setColorOption] = useState([]);
+  const [colorsOption, setColorsOption] = useState([]);
 
   useEffect(() => {
     dispatch(fetchColorsAction());
   }, [dispatch]);
 
   //select colors data from store
-  const { colors } = useSelector((state) => state?.colors);
+  const {
+    colors: { data: colors },
+  } = useSelector((state) => state?.colors);
 
-  let colorOptionsCoverted = colors?.data?.map((color) => {
+  const handleColorChange = (colors) => {
+    setColorsOption(colors);
+  };
+  //converted colors
+  const colorsCoverted = colors?.map((color) => {
     return {
-      value: color.name,
-      label: color.name,
+      value: color?.name,
+      label: color?.name,
     };
   });
 
-  const handleColorChangeOption = (color) => {
-    setColorOption(color);
-  };
-
-  //products
-  useEffect(() => {
-    dispatch(fecthProductsAction());
-  }, [dispatch]);
-
-  //fetch single product
-  useEffect(() => {
-    dispatch(fetchProductAtion(id));
-  }, [id, dispatch]);
-
-  const {
-    product: { data: product },
-    isUpdated: productIsUpdated,
-    loading: updateLoading,
-    error: updateError,
-  } = useSelector((state) => state?.products);
+  //get product from store
+  const { product, isUpdated, loading, error } = useSelector(
+    (state) => state?.products
+  );
 
   //---form data---
   const [formData, setFormData] = useState({
-    name: product?.name,
-    description: product?.description,
-    category: product?.category,
-    sizes: ["X"],
-    brand: product?.brand,
-    colors: product?.colors,
-    price: product?.price,
-    totalQty: product?.price,
+    name: product?.data?.name,
+    description: product?.data?.description,
+    category: product?.data?.category,
+    sizes: "",
+    brand: product?.data?.brand,
+    colors: product?.data?.colors,
+    price: product?.data?.price,
+    totalQty: product?.data?.totalQty,
   });
-
-  const { name, brand, category, description, totalQty, price } = formData;
 
   //onChange
   const handleOnChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const {
-    isAdded,
-    loading: loadingProduct,
-    error: errProductCreation,
-  } = useSelector((state) => {
-    return state?.products;
-  });
-
   //onSubmit
   const handleOnSubmit = (e) => {
     e.preventDefault();
-
-    //dispatch action create product
+    //dispatch
     dispatch(
       updateProductAction({
         ...formData,
         id,
-        // files,
-        colors: colorOption?.map((color) => color.value),
-        sizes: sizeOption?.map((size) => size.value),
+        colors: colorsOption?.map((color) => color.label),
+        sizes: sizeOption?.map((size) => size?.label),
       })
     );
 
     //reset form data
-    // setFormData({
-    //   name: "",
-    //   description: "",
-    //   category: "",
-    //   sizes: "",
-    //   brand: "",
-    //   colors: "",
-    //   images: "",
-    //   price: "",
-    //   totalQty: "",
-    // });
+    setFormData({
+      name: "",
+      description: "",
+      category: "",
+      sizes: "",
+      brand: "",
+      colors: "",
+      images: "",
+      price: "",
+      totalQty: "",
+    });
   };
 
   return (
     <>
-      {/* {errProductCreation && <ErrorMsg message={errProductCreation?.message} />} */}
-
-      {productIsUpdated && <SuccessMsg message="Produto atualizado" />}
+      {error && <ErrorMsg message={error?.message} />}
+      {isUpdated && <SuccessMsg message="Product Updated Successfully" />}
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-            Editar produto
+            Update Product
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             <p className="font-medium text-indigo-600 hover:text-indigo-500">
@@ -204,7 +162,6 @@ export default function ProductUpdate() {
                     name="name"
                     value={formData?.name}
                     onChange={handleOnChange}
-                    defaultValue="Nike Dunk"
                     className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   />
                 </div>
@@ -278,68 +235,21 @@ export default function ProductUpdate() {
                   components={animatedComponents}
                   isMulti
                   name="colors"
-                  options={colorOptionsCoverted}
+                  options={colorsCoverted}
                   className="basic-multi-select"
                   classNamePrefix="select"
                   isClearable={true}
                   isLoading={false}
                   isSearchable={true}
                   closeMenuOnSelect={false}
-                  onChange={(e) => handleColorChangeOption(e)}
+                  onChange={(e) => handleColorChange(e)}
                 />
               </div>
-
-              {/* upload images
-              <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                <label
-                  htmlFor="cover-photo"
-                  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-                >
-                  Upload Images
-                </label>
-                <div className="mt-1 sm:col-span-2 sm:mt-0">
-                  <div className="flex max-w-lg justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                    <div className="space-y-1 text-center">
-                      <svg
-                        className="mx-auto h-12 w-12 text-gray-400"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 48 48"
-                        aria-hidden="true"
-                      >
-                        <path
-                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                          strokeWidth={2}
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        />
-                      </svg>
-                      <div className="flex text-sm text-gray-600">
-                        <label
-                          htmlFor="file-upload"
-                          className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
-                        >
-                          <span>Upload files</span>
-                          <input
-                            name="images"
-                            value={formData.images}
-                            onChange={fileHandleChange}
-                            type="file"
-                          />
-                        </label>
-                      </div>
-                      <p className="text-xs text-gray-500">
-                        PNG, JPG, GIF até 1MB
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div> */}
 
               {/* price */}
               <div>
                 <label className="block text-sm font-medium text-gray-700">
-                  Preço
+                  Price
                 </label>
                 <div className="mt-1">
                   <input
@@ -355,7 +265,7 @@ export default function ProductUpdate() {
               {/* Quantity */}
               <div>
                 <label className="block text-sm font-medium text-gray-700">
-                  Quantidade total
+                  Total Quantity
                 </label>
                 <div className="mt-1">
                   <input
@@ -373,7 +283,7 @@ export default function ProductUpdate() {
                   htmlFor="comment"
                   className="block text-sm font-medium text-gray-700"
                 >
-                  Descrição
+                  Add Product Description
                 </label>
                 <div className="mt-1">
                   <textarea
@@ -386,26 +296,17 @@ export default function ProductUpdate() {
                 </div>
               </div>
               <div>
-                {updateLoading ? (
+                {loading ? (
                   <LoadingComponent />
                 ) : (
                   <button
-                    //disabled={filesError.length > 0 && true}
                     type="submit"
                     className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                   >
-                    Editar produto
+                    Update Product
                   </button>
                 )}
               </div>
-              {!updateLoading && updateError && (
-                <p className="text-red-500">{updateError?.message}</p>
-              )}
-              {/* {filesError.length > 0 && (
-                <p className="text-red-500">
-                  {filesError.map((error) => error)}
-                </p>
-              )} */}
             </form>
           </div>
         </div>
