e6c230606cf3f6ddc167f898a8ed9c2552e16f1a
Marcio <marciodosanjos@ymail.com>
2024-11-13 07:17:13 -0300

    redesign cart page

--- src/pages/Cart/ShoppingCart.js
+++ src/pages/Cart/ShoppingCart.js
@@ -18,68 +18,75 @@ import {
 } from "@mui/material";
 import { Add, Remove } from "@mui/icons-material";
 import { Trash2 } from "lucide-react";
+import useIsMobile from "../../hooks/useIsMobile";
+import Teaser from "../../components/Teaser/Teaser";
 
 export default function ShoppingCart() {
-  const [quantity, setQuantity] = useState(1);
-
   const dispatch = useDispatch();
+  const [quantities, setQuantities] = useState({});
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     dispatch(getCartItemsAction());
   }, [dispatch]);
 
   const { cartItems } = useSelector((state) => state?.cart);
-  console.log(cartItems);
 
-  //add to cart handler
   const changeOrderItemQtyHandler = (productId, qty) => {
     dispatch(changeCartQtyAction({ productId, qty }));
     dispatch(getCartItemsAction());
   };
 
-  //add to cart handler
   const removeOrderItemFromLocalStorageHandler = (productId) => {
     dispatch(removeItemCartAction(productId));
     dispatch(getCartItemsAction());
   };
 
-  //calculate total price
-  const sumTotalPrice = cartItems?.reduce((acc, curr) => {
-    return acc + curr?.totalPrice;
-  }, 0);
+  const handleQuantityChange = (productId, newQuantity) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [productId]: newQuantity,
+    }));
+    changeOrderItemQtyHandler(productId, newQuantity);
+  };
+
+  const sumTotalPrice = cartItems?.reduce(
+    (acc, curr) => acc + curr?.totalPrice,
+    0
+  );
 
   let data = [
-    {
-      title: "Subtotal",
-      value: 300,
-    },
-    {
-      title: "Frete",
-      value: "free",
-    },
-    {
-      title: "Taxa",
-      value: 20,
-    },
+    { title: "Subtotal", value: 300 },
+    { title: "Frete", value: "free" },
+    { title: "Taxa", value: 20 },
   ];
 
   return (
     <>
       <Container sx={{ marginY: 10 }}>
-        <Grid container sx={{ gap: 4 }}>
+        <Grid
+          container
+          direction="row"
+          sx={{
+            gap: 4,
+            justifyContent: "space-between",
+          }}
+        >
           {/* Produtos */}
-          <Grid item xs={12} sm={12} md={7} lg={7} sx={{ backgroundColor: "" }}>
+          <Grid item xs={12} sm={12} md={7} lg={7}>
             <Box
               sx={{
                 borderBottom: "0.5px solid rgba(0, 0, 0, 0.1)",
                 paddingY: 1,
               }}
             >
-              <Typography variant="h5">Seu carrinho</Typography>
+              <Typography variant="h5" sx={{ textAlign: isMobile && "center" }}>
+                Seu carrinho
+              </Typography>
             </Box>
-            {cartItems?.map((item, index) => (
+            {cartItems?.map((item) => (
               <Box
-                key={index}
+                key={item?._id}
                 sx={{
                   marginY: 3,
                   display: "flex",
@@ -89,40 +96,76 @@ export default function ShoppingCart() {
                 }}
               >
                 <Box>
-                  <img src={item?.image} alt="" height={150} width={150} />
+                  <img
+                    src={item?.image}
+                    alt={item?.name}
+                    height={150}
+                    width={150}
+                  />
                 </Box>
-                <Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingLeft: 2,
+                  }}
+                >
                   <Typography variant="h6">{item?.name}</Typography>
                   <Typography variant="body1">{item?.color}</Typography>
                   <Typography variant="body1">{item?.size}</Typography>
-                </Box>
-                <Box>
-                  <Typography variant="h6">R$ {sumTotalPrice}</Typography>
+                  <Typography variant="body2">R$ {item?.totalPrice}</Typography>
                 </Box>
                 <Box>
                   <Box display="flex" alignItems="center">
                     <IconButton
-                      onClick={(e) => {
-                        changeOrderItemQtyHandler(item?._id, quantity);
-                      }}
-                      disabled={quantity <= 1}
+                      onClick={() =>
+                        handleQuantityChange(
+                          item?._id,
+                          (quantities[item?._id] || 1) - 1
+                        )
+                      }
+                      disabled={(quantities[item?._id] || 1) <= 1}
                     >
                       <Remove />
                     </IconButton>
                     <TextField
-                      value={quantity}
-                      onClick={() => {
-                        setQuantity(quantity + 1);
-                      }}
+                      value={quantities[item?._id] || 1}
+                      onChange={(e) =>
+                        handleQuantityChange(
+                          item?._id,
+                          parseInt(e.target.value, 10) || 1
+                        )
+                      }
                       type="number"
                       inputProps={{ min: 1 }}
-                      sx={{ width: "60px", textAlign: "center", mx: 1 }}
+                      sx={{
+                        width: "3rem",
+                        display: "flex",
+                        alignItems: "center",
+                        "& input": {
+                          textAlign: "center",
+                        },
+                        "& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button":
+                          {
+                            WebkitAppearance: "none",
+                            margin: 0,
+                          },
+                      }}
+                      InputProps={{
+                        min: 1,
+                        style: {
+                          textAlign: "center",
+                          MozAppearance: "textfield",
+                        },
+                      }}
                     />
                     <IconButton
-                      onClick={() => {
-                        setQuantity(quantity + 1);
-                        changeOrderItemQtyHandler(item?._id, quantity);
-                      }}
+                      onClick={() =>
+                        handleQuantityChange(
+                          item?._id,
+                          (quantities[item?._id] || 1) + 1
+                        )
+                      }
                     >
                       <Add />
                     </IconButton>
@@ -148,15 +191,12 @@ export default function ShoppingCart() {
             sm={12}
             md={4}
             lg={4}
-            sx={{
-              backgroundColor: "",
-              borderRadius: 1,
-              boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
-              padding: 4,
-            }}
+            sx={{ boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)", padding: 4 }}
           >
             <Box sx={{ marginBottom: 6 }}>
-              <Typography variant="h4">Resumo</Typography>
+              <Typography variant="h4" sx={{ textAlign: isMobile && "center" }}>
+                Resumo
+              </Typography>
             </Box>
             <Box sx={{ borderBottom: "0.5px solid rgba(0, 0, 0, 0.1)" }}>
               {data.map((item, index) => (
@@ -164,7 +204,6 @@ export default function ShoppingCart() {
                   key={index}
                   sx={{
                     display: "flex",
-                    flexDirection: "row",
                     justifyContent: "space-between",
                     marginY: 3,
                   }}
@@ -184,7 +223,6 @@ export default function ShoppingCart() {
             <Box
               sx={{
                 display: "flex",
-                flexDirection: "row",
                 justifyContent: "space-between",
                 marginY: 3,
               }}
@@ -219,6 +257,19 @@ export default function ShoppingCart() {
           </Grid>
         </Grid>
       </Container>
+      <Teaser
+        title={"Ficou com dúvidas?"}
+        text={"Entre em contato com a gente e saiba mais"}
+        buttonText={"Entrar em contato"}
+        img={
+          "https://static.vecteezy.com/system/resources/thumbnails/041/714/543/small_2x/ai-generated-black-blank-t-shirt-front-mockup-on-a-transparent-background-png.png"
+        }
+        blockHeight={{ xs: "60vh", sm: "60vh", md: "40vh" }}
+        titleSize={"h3"}
+        imgHeight={233}
+        imgWidth={250}
+        destination={"/"}
+      />
     </>
   );
 }
